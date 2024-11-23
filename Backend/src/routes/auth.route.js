fdda4e744b7998724ae9c0fbb021fdf32416bf92
omjee247 <omjee247@gmail.com>
2024-11-24 00:09:51 +0530

    Backend setup, Database Setup, SignUp

---
+++ Backend/src/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { login, logout, signup } from "../controller/auth.controller.js";
+
+const router = express.Router();
+
+router.use(express.json())
+router.use(express.urlencoded({ extended: true }));
+
+router.post("/signup", signup);
+  
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+export default router;
