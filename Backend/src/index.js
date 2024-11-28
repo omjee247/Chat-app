669bae410604574306e8b58e4c1b82b463977dac
omjee247 <omjee247@gmail.com>
2024-11-28 23:13:41 +0530

    message routes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js"
 
 import { connectDB } from "./lib/db.js";
 
@@ -15,6 +16,8 @@ app.use(express.json())
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
+app.use("/api/messages", messageRoutes);
+
 
 app.listen(PORT, () => {
     console.log("Server is running on PORT: " + PORT);
